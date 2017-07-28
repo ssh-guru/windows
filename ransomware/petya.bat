2a21e35a2308215cb10b53468f550cb815661e21
vlku <vlku@null.net>
2017-07-28 10:45:15 +0100

    Update and rename nopetyavac.bat to ransomware/petya.bat

---
+++ ransomware/petya.bat
@@ -0,0 +1,13 @@
+#This script adds files which should disable Petya Ransomware & protect Windows machines from infection in the future
+
+net session >nul 2>&1
+
+
+echo This is a NotPetya/Petya/Petna/SortaPetya Vaccination file. Do not remove as it protects you from being encrypted by Petya. > C:\Windows\perfc
+                
+echo This is a NotPetya/Petya/Petna/SortaPetya Vaccination file. Do not remove as it protects you from being encrypted by Petya. > C:\Windows\perfc.dll
+
+echo This is a NotPetya/Petya/Petna/SortaPetya Vaccination file. Do not remove as it protects you from being encrypted by Petya. > C:\Windows\perfc.dat
+attrib +R C:\Windows\perfc
+attrib +R C:\Windows\perfc.dll
+attrib +R C:\Windows\perfc.dat
